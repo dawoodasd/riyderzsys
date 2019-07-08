c3ea92748633c08d977dd2b233847f866477ffe5
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-07-08 19:01:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '-'
+const devs = ['413660639668731914'];
 
 client.on('ready', () => {
     console.log(`~~~~~~~~~~~~~~~~~`);
