b3e5e5225af36873af595182ffe427f969a2304d
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-07-09 13:46:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2988,16 +2988,6 @@ client.on("message", message => { //iTzMurtaja#8951
 
 
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const xkiller = member.guild.channels.find("name", "🌈⇅chat-شــات");
-     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-  });
-});
-
 
 
  const cuttweet = [
