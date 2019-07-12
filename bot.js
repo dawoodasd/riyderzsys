b019895fb066f4dd11bc55730d090b5e22deb51c
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-07-13 00:25:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3144,5 +3144,52 @@ hours = 12;
 
 
 
+
+
+
+
+client.on('message', async message => {
+var prefix = '-' /// البريفكس هون غيره يا بعد قلبي
+if(message.content.startsWith( prefix + 'infoinvite')) { //// وهون الامر طبعا
+        let oi = message.mentions.users.first() ? message.mentions.users.first().id : message.author.id;
+        let Tag = message.mentions.users.first() ? message.mentions.users.first().tag : message.author.tag;
+        let Username = message.mentions.users.first() ? message.mentions.users.first().username : message.author.username;
+        let Avatar = message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL;
+       
+        message.guild.fetchInvites().then(invs => {
+            let member = client.guilds.get(message.guild.id).members.get(oi);
+            let personalInvites = invs.filter(i => i.inviter.id === oi);
+            let urll = invs.filter(i => i.inviter.id === oi);
+            let link = urll.reduce((p , v) => v.url +` , Total de membros recrutados no convite: ${v.uses}.\n`+ p, `\nServidor: ${message.guild.name} \n `);
+            let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+            let inviteCode = personalInvites.reduce((p, v) => v.code);
+            let possibleInvites = [['Total de membros recrutados:']];
+            possibleInvites.push([inviteCount, inviteCode]);
+            let user = message.mentions.users.first() || message.author;
+            let mem = message.guild.member(user);
+            let millisJoined = new Date().getTime() - mem.joinedAt.getTime();
+            let daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+           
+            var inviteInfo = new Discord.RichEmbed()
+            .setTitle(`:incoming_envelope: **[INVITE INFO]** ${Username}`)
+            .setThumbnail(client.user.avatarURL)
+            .addField('**عدد الدعوات للسيرفر**', `**➥** [ شخص **${Number(inviteCount)}** ]`)
+            .addField('**تاريخ انضمامك لسيرفرنا **', `**➥** [ منذ  **${daysJoined.toFixed(0)}** يوم ]`)
+            .addField('**رابط الدعوة الذي دخلت منه**', `**➥** [ **https://discord.gg/${inviteCode || 'Zm2U6we'}** ]`)
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setFooter(Tag, Avatar)
+           
+            message.channel.send(inviteInfo);
+            });
+    };
+});
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
 
