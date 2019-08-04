6d241749bf0d36b556935cdf98a62360f022add7
DAVIED <51955742+dawoodasd@users.noreply.github.com>
2019-08-04 22:21:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3489,7 +3489,68 @@ client.on('message',message =>{
 });
 
 
-
+client.on('message', message => {
+   
+    let u = convertMS(client.uptime);
+    let uptime = u.d + " days : " + u.h + " hours : " + u.m + " minutes : " + u.s + " seconds"
+   
+   
+    function convertMS(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+    return {
+        d: d
+        , h: h
+        , m: m
+        , s: s
+    };
+};
+ 
+    var api = `${Math.round(client.ping)}`
+ 
+ 
+ if (message.content.startsWith("-info")) {
+client.fetchUser('413660639668731914').then(user=>{
+ 
+ 
+      message.channel.send({
+ embed: new Discord.RichEmbed()
+    .setColor('#36393e')
+       
+        .addField('**PING**' , `${Date.now() - message.createdTimestamp}` + ' ms')
+        .addField('**RAM**' , `${~~(process.memoryUsage().heapUsed / 1024 / 1024)}mb` + '.')
+ 
+.addField('**WebSocket:**',api + " ms  ")
+       .addField(`:control_knobs: `, `**Uptime :**  ${uptime}`)
+               .setFooter(`Run by:
+${user.tag}
+`)
+            })
+ 
+   
+ 
+ 
+})
+ }
+  });
+ 
+  function timeCon(time) {
+    let days = Math.floor(time % 31536000 / 86400)
+    let hours = Math.floor(time % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+    days = days > 9 ? days : '0' + days
+    hours = hours > 9 ? hours : '0' + hours
+    minutes = minutes > 9 ? minutes : '0' + minutes
+    seconds = seconds > 9 ? seconds : '0' + seconds
+    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
+};
 
 
 client.login(process.env.BOT_TOKEN);
